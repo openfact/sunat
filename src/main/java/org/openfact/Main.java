70394b84226e07811431ae3c564a0aa8c5fd4fab
Carlos Feria <carlosthe19916@gmail.com>
2017-10-10 17:58:24 -0500

    remove main

--- src/main/java/org/openfact/Main.java
+++
@@ -1,8 +0,0 @@
-package org.openfact;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hi there! I was forged as part of the project you call sunat.");
-    }
-}
