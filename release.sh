262d9b6bc7b87d4dee41226127bf0a42b70b668b
Carlos Feria <carlosthe19916@gmail.com>
2017-10-10 17:35:36 -0500

    versioning

--- release.sh
+++ release.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-read -s -p "Enter Github Username: " username
+echo "Configuring release \n"
+read -s "Enter Release: " releaseVersion
+echo "\n"
+read -s "Enter Next Release: " nextVersion
+echo "\n"
+read -s "Enter Github Username: " username
+echo "\n"
 read -s -p "Enter Github Password: " password
-
-releaseVersion=1.0.3
-nextVersion=1.0.4
+echo "\n"
 
 # Update to release
 mvn versions:set -DnewVersion="$releaseVersion"
@@ -15,11 +19,11 @@ git push https://`$username`:`$password`@github.com/openfact/sunat.git
 
 # Create tag and push
 git tag "$releaseVersion"
-git push --tags
+git push https://`$username`:`$password`@github.com/openfact/sunat.git --tags
 
 # Create next snapshot
 mvn versions:set -DnewVersion="$nextVersion-SNAPSHOT"
 
 git add .
 git commit -m "Prepare next release $nextVersion-SNAPSHOT"
-git push
+git push https://`$username`:`$password`@github.com/openfact/sunat.git
