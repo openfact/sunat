bd2927a4b509fedcf798a816d41f07bc6490aefd
Carlos Feria <carlosthe19916@gmail.com>
2017-10-10 17:20:14 -0500

    versioning

--- release.sh
+++ release.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
+releaseVersion=1.0.3
+nextVersion=1.0.4
+
 # Update to release
-mvn versions:set -DnewVersion=1.0.2
+mvn versions:set -DnewVersion="$releaseVersion"
 
 git add .
-git commit -m 'Release 1.0.2'
-git push
+git commit -m "Prepare release $releaseVersion"
+git push https://`$1`:`$2`@github.com/openfact/sunat.git
 
 # Create tag and push
-git tag 1.0.2
+git tag "$releaseVersion"
 git push --tags
 
 # Create next snapshot
-mvn versions:set -DnewVersion=1.0.3-SNAPSHOT
+mvn versions:set -DnewVersion="$nextVersion-SNAPSHOT"
 
 git add .
-git commit -m 'Prepare next release 1.0.3-SNAPSHOT'
+git commit -m "Prepare next release $nextVersion-SNAPSHOT"
 git push
