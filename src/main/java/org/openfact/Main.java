05975af869d32ab27fe651d7e6eb075e61a6ef62
Carlos Feria <carlosthe19916@gmail.com>
2017-10-10 13:01:51 -0500

    fled(template): push first template

---
+++ src/main/java/org/openfact/Main.java
@@ -0,0 +1,8 @@
+package org.openfact;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hi there! I was forged as part of the project you call sunat.");
+    }
+}
